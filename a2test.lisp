60912e6ed9198c34b5694f4ad9286e074300d13e
Yuan Wang <wang17@ualberta.ca>
2019-02-09 15:05:49 -0700

    first commit

---
+++ a2test.lisp
@@ -0,0 +1,81 @@
+; Cmput 325 Winter 2019 Assignment 2
+; Self Public test cases
+; Yuan Wang
+
+; How to use:
+; First, load your assignment solution into SBCL
+; Second, load this file.
+; Third, fix the bugs and run this file again
+; You can also copy+paste individual tests from here into SBCL
+
+(defun test-case (ID Test Result)
+    (if (equal Test Result)
+        (format t "Test ~S OK~%" ID)
+        (format t "FAIL: Test ~S expected ~S got ~S~%" ID Result Test)
+    )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 1. remove-identities
+
+; basic tests for +
+(test-case '1.1.1 (remove-identities '(+ x 1)) '(+ x 1))
+(test-case '1.1.2 (remove-identities '(+ 1 0)) 1)
+(test-case '1.1.3 (remove-identities '(+ 0 1)) 1)
+(test-case '1.1.4 (remove-identities '(+ 1 x)) '(+ 1 x))
+
+(test-case '1.1.5 (remove-identities '(+ (+ x 1) 1)) '(+ (+ x 1) 1))
+(test-case '1.1.6 (remove-identities '(+ (+ 1 x) 0)) '(+ 1 x))
+(test-case '1.1.7 (remove-identities '(+ 0 (+ x 1))) '(+ x 1))
+(test-case '1.1.8 (remove-identities '(+ (+ 1 1) 0)) '(+ 1 1))
+(test-case '1.1.9 (remove-identities '(+ 0 (+ 1 1))) '(+ 1 1))
+
+(test-case '1.1.10 (remove-identities '(+ (+ 2 0) 0)) '2)
+(test-case '1.1.11 (remove-identities '(+ (+ 0 3) (+ x 3))) '(+ 3 (+ x 3)))
+(test-case '1.1.12 (remove-identities '(+ 0 (+ (+ x 0) 0))) 'x)
+(test-case '1.1.13 (remove-identities '(+ (+ 2 2) (+ x 0))) '(+ (+ 2 2) x))
+(test-case '1.1.13 (remove-identities '(+ (+ 1 0) (+ 0 x))) '(+ 1 x))
+(test-case '1.1.14 (remove-identities '(+ (+ (+ 0 x) 0) (+ 2 x))) '(+ x (+ 2 x)))
+
+; basic tests for -
+(test-case '1.2.1 (remove-identities '(- 1 0)) 1)
+(test-case '1.2.2 (remove-identities '(- 0 1)) '(- 0 1))
+(test-case '1.2.3 (remove-identities '(- 1 1)) '(- 1 1))
+(test-case '1.2.4 (remove-identities '(- 0 0)) 0)
+(test-case '1.2.5 (remove-identities '(- 2 1)) '(- 2 1))
+(test-case '1.2.6 (remove-identities '(- 2 2)) '(- 2 2))
+
+(test-case '1.2.7 (remove-identities '(- (- x 0) 1)) '(- x 1))
+(test-case '1.2.8 (remove-identities '(- (- 0 x) 0)) '(- 0 x))
+(test-case '1.2.9 (remove-identities '(- (- (- 1 1) (- 2 0)) 0)) '(- (- 1 1) 2))
+(test-case '1.2.10 (remove-identities '(- (- x x) (- x 1))) '(- (- x x) (- x 1)))
+(test-case '1.2.11 (remove-identities '(- (- x 0) (- x 0))) '(- x x))
+
+(test-case '1.2.12 (remove-identities '(- (- 2 0) 0)) 2)
+(test-case '1.2.13 (remove-identities '(- (- 0 x) 2)) '(- (- 0 x) 2))
+(test-case '1.2.14 (remove-identities '(- (- 2 1) (- (- x 0) 0))) '(- (- 2 1) x))
+
+; basic tests for *
+(test-case '1.3.1 (remove-identities '(* x 2)) '(* x 2))
+(test-case '1.3.2 (remove-identities '(* 2 x)) '(* 2 x))
+(test-case '1.3.3 (remove-identities '(* 5 0)) '(* 5 0))
+(test-case '1.3.4 (remove-identities '(* 1 0)) 0)
+(test-case '1.3.5 (remove-identities '(* 0 1)) 0)
+(test-case '1.3.6 (remove-identities '(* 0 x)) '(* 0 x))
+(test-case '1.3.7 (remove-identities '(* x 0)) '(* x 0))
+
+
+(test-case '1.3.8 (remove-identities '(* 2 1)) 2)
+(test-case '1.3.9 (remove-identities '(* 1 3)) 3)
+(test-case '1.3.10 (remove-identities '(* (* 1 1) 0)) 0)
+(test-case '1.3.11 (remove-identities '(* (* 1 1) (* 3 1))) 3)
+(test-case '1.3.12 (remove-identities '(* (* 3 1) (* x 0))) '(* 3 (* x 0)))
+(test-case '1.3.13 (remove-identities '(* (* x 0) (* x 1))) '(* (* x 0) x))
+(test-case '1.3.14 (remove-identities '(* (* 1 x) (* (* x 1) (* 2 1)))) '(* x (* x 2)))
+
+; basic tests for mixing +, -, *
+(test-case '1.4.1 (remove-identities '(+ 0 (* x 1))) 'x)
+(test-case '1.4.2 (remove-identities '(+ (+ 0 0) (* 5 1))) 5)
+(test-case '1.4.3 (remove-identities '(- 10 (+ 0 (* 0 1)))) 10)
+(test-case '1.4.4 (remove-identities '(- 10 10)) '(- 10 10))
+(test-case '1.4.5 (remove-identities '(- 10 10)) '(- 10 10))
